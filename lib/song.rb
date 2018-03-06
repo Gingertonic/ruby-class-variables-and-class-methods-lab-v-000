c92c1542d723d54c2fa86b85f5323e264124a249
Beth Schofield <bethmschofield@gmail.com>
2018-03-06 00:16:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,31 @@
 class Song
+ attr_reader :name :artist :genres
 
+ ARTISTS = Array.new
+ GENRES = Array.new
+ @@artist_count = 0
+ @@genre_count = 0
+
+  def initialize(title, artist, genre)
+    @name = title
+    @artist = artist
+    @genre = genre
+    if ARTISTS.include?(artist) 
+      return "Already here!"
+    else
+      ARTISTS << artist
+      @@artist_count += 1
+    end
+    if GENRES.include?(genre) 
+      return "Already here!"
+    else
+      GENRES << genre
+      @@genre_count += 1
+    end
+    
+        
+        
+  end
+    
+  end
 end
